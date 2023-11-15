d74eb964c74f13a4bc47b138afa56418dc23da7d
Roald andre kvarv <84967397+rakvarv@users.noreply.github.com>
2023-11-15 10:42:59 +0100

    Update queryhandler.py
    Added comments to the query handler.

--- utils/queryhandler.py
+++ utils/queryhandler.py
@@ -3,19 +3,26 @@ import json
 import os, sys
 from config import Config
 
+# Define a base class for sending queries to the OpenAI API
 class Query:
+    # Initialize the class with the model and API key
     def __init__(self):
         self.model = Config.OPENAI_MODEL
         self.api_key = Config.SECRET_KEY
 
+    # Define a method for sending a query
+    # This method is not implemented in this base class and must be implemented in any subclasses
     def send_query(self, prompt: str):
         raise NotImplementedError("Subclasses must implement this method.")
     
+# Define a subclass for sending chat completion queries that return JSON responses
 class ChatCompletionJSON(Query):
+    # Initialize the class with the number of tokens
     def __init__(self, tokens: int):
         super().__init__()
         self.tokens = tokens 
 
+    # Implement the send_query method for this subclass
     def send_query(self, prompt: str):
         openai.api_key = self.api_key
         full_prompt = Config.JSON_PROMPT
@@ -27,26 +34,31 @@ class ChatCompletionJSON(Query):
         )
         api_response = response.choices[0].text.strip()
 
+        # Check if the API response is a string
         if isinstance(api_response, str):
             try:
+                # Try to parse the string as JSON
                 api_response = json.loads(api_response)
                 print(api_response)
 
+                # Extract the locations and relationships from the API response
                 locations = [loc['name'] for loc in api_response['locations']]
                 relationships = [{'source': rel['source'], 'relation': rel['relation'], 'target': rel['target']} for rel in api_response['relationships']]
 
                 return api_response, locations, relationships
             except json.JSONDecodeError:
-                print("THe API response is not valid JSOn.")
+                print("The API response is not valid JSON.")
         else:
-            print("THe API response is already a python Dictionary, no need to parse it.")
+            print("The API response is already a python Dictionary, no need to parse it.")
 
-
+# Define a subclass for sending chat completion queries that return other types of responses
 class ChatCompletionOther(Query):
+    # Initialize the class with the number of tokens
     def __init__(self, tokens: int):
         super().__init__()
         self.tokens = tokens 
 
+    # Implement the send_query method for this subclass
     def send_query(self, prompt: str):
         openai.api_key = self.api_key
         full_prompt = Config.OTHER_PROMPT
@@ -57,5 +69,6 @@ class ChatCompletionOther(Query):
             max_tokens = Config.MAX_TOKENS
         )
 
-        api_response = response.choice[0].text.strip()
+        # Extract the API response
+        api_response = response.choices[0].text.strip()
         return api_response
