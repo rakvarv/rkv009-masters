adce861f65b83214d42e2b3819ceeb03ecfce57d
Roald andre kvarv <84967397+rakvarv@users.noreply.github.com>
2023-11-15 10:40:37 +0100

    Update graphhandler.py
    Added discriptive comments to the graph handler.

--- utils/graphhandler.py
+++ utils/graphhandler.py
@@ -3,46 +3,56 @@ import rdflib
 import kglab
 from config import Config
 
-
+# Define a class for creating a knowledge graph
 class KnowledgeGraphCreator:
+    # Initialize the class with locations and relationships
     def __init__(self, locations, relationships):
         self.locations = locations 
         self.relationships = relationships
-        self.kg = kglab.KnowledgeGraph()
+        self.kg = kglab.KnowledgeGraph()  # Create an instance of a knowledge graph
 
+    # Define a method for creating the knowledge graph
     def create_knowledge_graph(self):
+        # Check if the turtle file exists
         if os.path.isfile("master_thesis.ttl"):
-            self.kg.load_rdf("master_thesis.ttl")
+            self.kg.load_rdf("master_thesis.ttl")  # Load the existing graph
             print("previous RDF loaded")
 
+        # Define the namespaces
         LOC = rdflib.Namespace("http://example.org/locations/")
         REL = rdflib.Namespace("http://example.org/relationships/")
 
+        # Add the namespaces to the knowledge graph
         self.kg.add_ns("loc", LOC)
         self.kg.add_ns("rel", REL)
 
-        # add locations to graph
+        # Add locations to the graph
         for location in self.locations:
             # Check if the location already exists in the graph
             if (LOC[location], rdflib.RDF.type, LOC.location) not in self.kg.rdf_graph():
-                self.kg.add(LOC[location], rdflib.RDF.type, LOC.location)
+                self.kg.add(LOC[location], rdflib.RDF.type, LOC.location)  # Add the location if it doesn't exist
 
-        # Add relationships to graph:
+        # Add relationships to the graph
         for relationship in self.relationships:
             source = LOC[relationship['source']]
             target = LOC[relationship['target']]
             relation = REL[relationship['relation']]
-            self.kg.add(source, relation, target)
+            self.kg.add(source, relation, target)  # Add the relationship
 
+        # Try to save the graph
         try:
             self.kg.save_rdf("master_thesis.ttl")
         except Exception as e:
             print("RDF Failed To Save...")
         
+        # Create a subgraph tensor
         subgraph = kglab.SubgraphTensor(self.kg)
         graph_style = Config.GRAPH_STYLE
+        # Build a PyVis graph from the subgraph tensor
         pyvis_graph = subgraph.build_pyvis_graph(notebook="true", style=graph_style)
         pyvis_graph.force_atlas_2based
+        # Show the graph
         pyvis_graph.show("kg.html")
 
+        # Return the PyVis graph
         return pyvis_graph
